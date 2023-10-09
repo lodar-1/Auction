65c1408b76f46488b70cf620341a15536a663bcf
Daryl <lodard@gmail.com>
2023-10-09 20:47:49 +0200

    added watchlist functionality

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:viewtype>", views.index, name="index"),
+    path("watchlist", views.index, name="viewwatchlist"),
     path("listing/view/<int:listingid>", views.viewlisting, name="viewlisting"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
@@ -13,5 +15,7 @@ urlpatterns = [
     path("listing/<str:formtype>/<int:listingid>", views.newlisting, name="listing"),
     path("bid/<int:listingid>", views.bid, name="bid"),
     path("comment/<int:listingid>", views.comment, name="comment"),
+    path("watchlist/<int:listingid>", views.watchlist, name="watchlist"),
+    path("watchlist/<int:listingid>/<int:remove>", views.watchlist, name="watchlist"),
  
 ]
