43029f498803ee8cf2d79f8293c4556690ac93dc
Daryl <lodard@gmail.com>
2023-10-11 21:50:44 +0200

    added category selection, bid validation, closed auction status, fixed authorization issues

--- project2/commerce/auctions/views.py
+++ project2/commerce/auctions/views.py
@@ -10,14 +10,20 @@ from django.db import connection
 
 
 defaultimage = "/static/auctions/images/default.png" 
+watchcnt=10
 
+def getwatchcount(user):
+	if user.is_authenticated:
+		return Watchlist.objects.filter(user_id = user).count()
+	else:
+		return 0
+		
 def dictfetchall(cursor):
 
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
-def index(request, viewtype='active'):
-	print(defaultimage)
+def index(request, viewtype='active', categoryid=-1):
 	title = ""
 	userid = "0"
 	userauth = False
@@ -32,26 +38,40 @@ def index(request, viewtype='active'):
 		return newlisting(request, 'add')
 	if viewtype=='active':
 		title = "Active Listings"
-		cursor.execute("""select l.id 'id', l.title 'title', l.image_link, l.description, l.listing_date, l.startbid, max(b.bid_amount) 'bid_amount', count(b.listing_id) 'bid_count', case when w.id is not null then 'true' else 'false' end 'watchlist' 
+		cursor.execute("""select l.id 'id', l.title 'title', l.image_link, l.description, l.listing_date, l.startbid, l.active, max(b.bid_amount) 'bid_amount', count(b.listing_id) 'bid_count', case when w.id is not null then 'true' else 'false' end 'watchlist' 
 					from auctions_listing l 
 					left join auctions_listingbid b on l.id = b.listing_id
 					left join auctions_watchlist w on w.listing_id_id  = l.id and w.user_id_id = %s
 					where l.active = true
-					group by l.id,b.listing_id, w.id;""", [userid])		
+					group by l.id,b.listing_id, w.id;""" %(userid))		
+	elif viewtype=='category':
+		category = Category.objects.get(id=categoryid)
+		title = f"{category.category_name} Listings"
+		cursor.execute("""select l.id 'id', l.title 'title', l.image_link, l.description, l.listing_date, l.startbid, l.active, max(b.bid_amount) 'bid_amount', count(b.listing_id) 'bid_count', case when w.id is not null then 'true' else 'false' end 'watchlist' 
+			from auctions_listing l 
+			left join auctions_listingbid b on l.id = b.listing_id
+			left join auctions_watchlist w on w.listing_id_id  = l.id and w.user_id_id = %s
+			inner join auctions_category c on l.category_id_id = c.id
+			where l.active = true
+			and c.id = %d
+			group by l.id,b.listing_id, w.id;""" %(userid, categoryid))	
 	else: 
 		if request.user.is_authenticated:
 			title = "Watchlist"
-			cursor.execute("""select l.id 'id', l.title 'title', l.image_link, l.description, l.listing_date, l.startbid, max(b.bid_amount) 'bid_amount', count(b.listing_id) 'bid_count' 
+			cursor.execute("""select l.id 'id', l.title 'title', l.image_link, l.description, l.listing_date, l.startbid, l.active, max(b.bid_amount) 'bid_amount', count(b.listing_id) 'bid_count' 
 					from auctions_listing l 
 					left join auctions_listingbid b on l.id = b.listing_id 
 					inner join auctions_Watchlist w on w.listing_id_id = l.id 
 					where w.user_id_id = %s 
-					group by l.id,b.listing_id;""", [userid])		
+					group by l.id,b.listing_id;""" %(userid))		
 		else:
 			return render(request, "auctions/login.html", {"message": "Login required."})
 	listings = dictfetchall(cursor)  
 	
-	return render(request, "auctions/index.html", {"listings": listings, "userauth":userauth, "title": title, "viewtype":viewtype})
+	renderdict = {"listings": listings, "userauth":userauth, "title": title, "viewtype":viewtype}
+	renderdict['watchcnt']=getwatchcount(request.user)
+	return render(request, "auctions/index.html", renderdict)
+	
 	#else:
 		#return render(request, "auctions/login.html", {"message": "Login required."})
 	#	return HttpResponseRedirect(reverse("index"))
@@ -138,20 +158,25 @@ def newlisting(request, formtype, listingid=None):
 		categories = Category.objects.all().order_by('category_name').values()
 		formlabel = "Edit"
 		listing = Listing.objects.get(id=listingid)
-		print(listing.description)
-		return render(request, "auctions/newlisting.html", { "categories": categories, "formlabel": formlabel, "scontent": listing.description, 
-		"title": listing.title, "selectedcategory": listing.category_id, "image": listing.image_link, "formtype": formtype, "listingid":listingid, "startbid":listing.startbid})	
+		renderdict = { "categories": categories, "formlabel": formlabel, "scontent": listing.description, "title": listing.title, 
+		"selectedcategory": listing.category_id, "image": listing.image_link, "formtype": formtype, "listingid":listingid, 
+		"startbid":listing.startbid}
+		renderdict['watchcnt'] = getwatchcount(request.user)
+		return render(request, "auctions/newlisting.html", renderdict)
 	else:	
 		categories = Category.objects.all().order_by('category_name').values()
 		formlabel = "Create"
-		return render(request, "auctions/newlisting.html", { "categories": categories, "formlabel": formlabel, "formtype": formtype})	
+		renderdict = { "categories": categories, "formlabel": formlabel, "formtype": formtype}
+		renderdict['watchcnt'] = getwatchcount(request.user)
+		return render(request, "auctions/newlisting.html", renderdict)	
 
 def viewlisting(request, listingid):
 	listing = Listing.objects.get(id=listingid)
-	listingbids = ListingBid.objects.filter(listing = listing)
+	listingbids = ListingBid.objects.filter(listing = listing).order_by('bid_amount')
 	bidcount = listingbids.count()
 	bwatchlist = False
 	userauth = False
+	minbid = 0
 	if request.user.is_authenticated:
 		userauth = True
 		#listinguser = Listing.user_id
@@ -162,18 +187,25 @@ def viewlisting(request, listingid):
 	if(bidcount > 0):
 		lastbid = listingbids.last().bid_amount
 		lastbiduser = listingbids.last().user_id
+		minbid = lastbid+1
 	else:
 		lastbid = 0
 		lastbiduser = ""
+		minbid = listing.startbid
+		
 	comments = Comment.objects.filter(listing_id = listing)
-	return render(request, "auctions/viewlisting.html", {"listing": listing, "userauth":userauth, "watchlist": bwatchlist, "listingbids": lastbid, "comments":comments, "bidscount":bidcount, "lastbiduser":lastbiduser})
+	print(f"last bid: {lastbiduser} current user: {request.user.id}")
+	renderdict = {"listing": listing, "userauth":userauth, "watchlist": bwatchlist, "minbid": minbid, "listingbids": lastbid, "comments":comments, "bidscount":bidcount, "lastbiduser":lastbiduser}
+	renderdict['watchcnt'] = getwatchcount(request.user)
+	return render(request, "auctions/viewlisting.html", renderdict)
 	
 @login_required(login_url="/login")
 def bid(request, listingid):
 	if request.method == "POST":	
 		listing = Listing.objects.get(id=listingid)
 		listbid = ListingBid.objects.create(bid_amount=request.POST["txtBid"], bid_datetime=datetime.now(), listing_id = listingid, user_id=request.user)
-		return viewlisting(request, listingid) 
+		return watchlist(request, listingid, 4)
+		#return viewlisting(request, listingid) 
 	else:
 		return viewlisting(request, listingid) 
 		
@@ -188,12 +220,11 @@ def comment(request, listingid):
 		
 @login_required(login_url="/login")
 def watchlist(request, listingid, remove=0):
-		print(request)
 		listing = Listing.objects.get(id=listingid)
 		if remove > 0 and remove < 4:
 			Watchlist.objects.filter(user_id=request.user, listing_id = listing).delete()
 		elif remove == 4 or remove==0:	
-			Watchlist.objects.create(user_id=request.user, listing_id = listing)
+			Watchlist.objects.get_or_create(user_id=request.user, listing_id = listing)
 		if remove >= 3:
 			return viewlisting(request, listingid)
 		if remove == 1:
@@ -201,6 +232,20 @@ def watchlist(request, listingid, remove=0):
 		else:
 			return index(request, 'active')	
 
+def categories(request, categoryid=-1):
+	if categoryid > -1:
+		return index(request, "category", categoryid)	
+	else:	
+		cursor = connection.cursor()
+		cursor.execute("""select c.category_name, c.id, count(l.id) count from auctions_category c left join auctions_listing l on l.category_id_id = c.id 
+						and l.active=true
+						where c.category_name <> '<SELECT>'
+						group by l.category_id_id,c.category_name, c.id""")
+		categories = dictfetchall(cursor) 
+		renderdict= {"categories":categories}
+		renderdict['watchcnt'] = getwatchcount(request.user)
+		return render(request, "auctions/categories.html",renderdict)			
+	
 def close(request, listingid):
 	if request.method == "POST":
 		listing = Listing.objects.get(id=listingid)
