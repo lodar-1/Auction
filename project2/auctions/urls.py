25d3e4dbd1b5030f796b93679aec18b69acc9a51
Daryl <lodard@gmail.com>
2023-10-18 09:56:18 +0200

    changed project2 dir structure

---
+++ project2/auctions/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("categories/<int:categoryid>", views.categories, name="categories"),
+    path("categories", views.categories, name="categories"),
+    path("register", views.register, name="register"),
+    path("<str:viewtype>", views.index, name="index"),
+    path("watchlist", views.index, name="viewwatchlist"),
+    path("listing/view/<int:listingid>", views.viewlisting, name="viewlisting"),
+    path("<str:formtype>", views.newlisting, name="newlisting"), 
+    path("listing/<str:formtype>", views.newlisting, name="listing"),
+    path("listing/<str:formtype>/<int:listingid>", views.newlisting, name="listing"),
+    path("bid/<int:listingid>", views.bid, name="bid"),
+    path("comment/<int:listingid>", views.comment, name="comment"),
+    path("close/<int:listingid>", views.close, name="close"),
+    path("watchlist/<int:listingid>", views.watchlist, name="watchlist"),
+    path("watchlist/<int:listingid>/<int:remove>", views.watchlist, name="watchlist"),
+ 
+]
