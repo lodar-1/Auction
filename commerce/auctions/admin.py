c627b17eedcb90150a6ae27ae8a02bd732d24cdb
Daryl <lodard@gmail.com>
2023-10-18 10:35:04 +0200

    changed directory structure

--- commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Listing, ListingBid, Comment, Watchlist, Category
-
-# Register your models here.
-admin.site.register(Listing)
-admin.site.register(ListingBid)
-admin.site.register(Comment)
-admin.site.register(Watchlist)
-admin.site.register(Category)
